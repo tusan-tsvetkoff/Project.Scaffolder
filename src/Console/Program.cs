410f6faf919c5b77a43195ab5725ab59ad2f5cd7
Iliyan Tsvetkov <iliancetkov@gmail.com>
2023-11-20 20:05:14 +0200

    Add File Explorer proj to gitignore, refactor program.cs

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -2,15 +2,26 @@
 using ProjectScaffold.Builders;
 using ProjectScaffold.View;
 using FileExplorer.Main;
+using Spectre.Console;
+using System.Collections;
 
-// var solutionBuilder = new SolutionBuilder();
-// var directoryBuilder = new DirectoryBuilder();
-// var testBuilder = new TestBuilder();
-// var projectMaker = new ProjectMaker();
+namespace ProjectScaffold.Main;
 
-// var gui = new Gui(solutionBuilder, directoryBuilder, testBuilder, projectMaker);
-// await gui.Run();
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await RunProjectScaffolder();
+    }
 
+    internal static async Task RunProjectScaffolder()
+    {
+        var solutionBuilder = new SolutionBuilder();
+        var directoryBuilder = new DirectoryBuilder();
+        var testBuilder = new TestBuilder();
+        var projectMaker = new ProjectMaker();
 
-var fe = new FileTreeExplorer(@"C:\");
-fe.Run();
+        var gui = new Gui(solutionBuilder, directoryBuilder, testBuilder, projectMaker);
+        await gui.Run();
+    }
+}
